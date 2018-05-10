e42b8c26dcbc2b572ec0b18fc130fbdb76f3eafc
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-10 02:05:14 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,5 @@
-age_of_person = current_age_for_birth_year(1984)
+
 def current_age_for_birth_year(1984)
-  puts 2003 - #{1984}
+  puts "2003 - #{1984}"
 end
-current_age_for_birth_year
+current_age_for_birth_year (1984)
